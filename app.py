7200b88ecb21b07ecf363a22d6bdc41f2e461fec
Davis <charles.d.snead@gmail.com>
2025-04-25 14:37:56 -0400

    added port handling

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
+import os
 
 app = Flask(__name__)
 DB_PATH = 'scoreboard.db'
@@ -48,5 +49,8 @@ def remove():
     conn.close()
     return redirect('/')
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
